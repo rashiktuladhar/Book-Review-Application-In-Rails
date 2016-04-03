6c76a02a4cdc997bf09ef7865fd13bad9225e65d
Rashik <rashik.tuladhar@outlook.com>
2016-04-03 14:06:46 +0545

    added new and create method together with private action for params in books_controller, added a new form remplate to create a book and rendered it in new view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,22 @@ class BooksController < ApplicationController
 def index
 end
 
+# new book
+def new
+	@book = Book.new
+end
+
+# create book with param
+def create
+	@book = Book.new(book_params)
+end
+
+
+# private functions
+private
+
+def book_params
+	params.require(:book).permit(:title, :description, :author)
+end
+
 end
