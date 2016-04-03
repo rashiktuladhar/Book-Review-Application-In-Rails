cd818d4c96cd335f57bf43423e0212cbbf95c909
Rashik <rashik.tuladhar@outlook.com>
2016-04-03 14:31:05 +0545

    edit operation completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,20 @@
 class BooksController < ApplicationController
 
+
+# do this before calling any action below (need to call the action find book within private area for only the 4 operation mentioned before calling index and other action)
+before_action :find_book, only: [:show, :edit, :update, :destroy]
+
+
 # root route action
 def index
 	@books = Book.all.order("created_at DESC")
 end
+
+
+# show book details
+def show
+end
+
 
 # new book
 def new
@@ -21,11 +32,38 @@ def create
 end
 
 
+def edit
+end
+
+def update
+	if @book.update(book_params)
+		redirect_to book_path(@book)
+	else
+		render 'edit'
+	end
+end
+
+def destroy
+end
+
+
+
 # private functions
 private
 
+
+# available books parameters to add and update
 def book_params
 	params.require(:book).permit(:title, :description, :author)
 end
+
+
+# params for getting the id of the book for DRY (delete, update and show)
+def find_book
+	@book = Book.find(params[:id])
+end
+
+
+
 
 end
